a26178c1e164b47feef127d08c7a48b34b5f7700
Michael Cheng <michael.zhang.cheng@gmail.com>
2015-05-31 18:58:38 -0500

    js process almost there

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+    // just Node?
+    // var fetch = require('node-fetch')
+    // Browserify?
+require('whatwg-fetch') //--> not a typo, don't store as a var
 
 // es6 polyfills, powered by babel
 require("babel/register")
@@ -18,7 +18,27 @@ require("babel/register")
 // window.addEventListener('load', app)
 
 // function app() {
-    // start app
-    // new Router()
+// start app
+// new Router()
 // }
+var pullProf = (name) => fetch('https://api.github.com/users/' + name).then((data) => data.json())
+var pullRepos = (name) => fetch(`https://api.github.com/users/${name}/repos`).then((data) => data.json())
+var profElements = ['name', 'location', 'login', 'html_url', 'email', 'blog']
+var profile = (obj) => profElements.reduce((a, v, i) => a[v] = obj[v], {})
+var repoArr = (arr) => arr.map((repo) => repo.name)
+var formatLi = (name) => `<li>${name}</li>`
+var formatUl = (arr) => arr.map(formatLi(v)).reduce((a, v, i) => i < arr.length ? (a + v) : (a + v) + '</ul>', '<ul class="repos">')
+var formatProf = (prof) => 
+`<div class ="info"><pre>name: ${prof.name}
+location: ${prof.location}
+login: ${prof.login}
+html_url: ${prof.html_url}
+email: ${prof.email}
+blog: ${prof.blog}<pre><div>`
 
+var profPipe = [pullProf, profile, formatProf]
+var repoPipe = [pullRepos, repoArr, formatUl]
+
+var GithubClient = (name) => "<div class='profile'>"+ profPipe.reduce((a,v,i) => v(a), name) + repoPipe.reduce((a,v,i) => v(a), name) + "</div>"
+var michael = GithubClient('APartingGlass')
+michael.appendTo('.container')
